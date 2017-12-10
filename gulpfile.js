c325def177980dce8c60d80eafd1c31b70dd5dd3
Aleksander Pentchev <aleksander.pentchev@execon.pl>
2017-12-10 14:16:02 +0100

    Added new gulp tasks,: minify, uglify etc., completed dist task sequence

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
-var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
@@ -11,6 +10,9 @@ var del = require('del');
 var babel = require('gulp-babel');
 var runSequence = require('run-sequence');
 var autoprefixer = require('gulp-autoprefixer');
+var processhtml = require('gulp-processhtml');
+var htmlmin = require('gulp-htmlmin');
+var rename = require('gulp-rename')
 
 gulp.task('browserSync' , function() {
     browserSync.init({
@@ -20,20 +22,22 @@ gulp.task('browserSync' , function() {
     })
 })
 
-gulp.task('sass' , function () {
+gulp.task('sass-minify' , function () {
     return gulp.src('src/scss/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(cssnano())
         .pipe(gulp.dest('src/temp/css'))
         .pipe(browserSync.reload({
             stream: true
           }))
 })
 
-gulp.task('babel' , function(){
+gulp.task('babel-uglify' , function(){
     return gulp.src('src/js/*.js')
         .pipe(babel({
             presets: ['env']
         }))
+        .pipe(uglify())
         .pipe(gulp.dest('src/temp/js'))
 })
 
@@ -46,9 +50,28 @@ gulp.task('autoprefixer' , function() {
 })
 
 gulp.task('images', function() {
-    return gulp.src('src/img/*.+(png|jpg|gif|svg')
+    return gulp.src('src/img/**/*')
         .pipe(cache(imagemin()))
-        .pipe(gulp.dest('temp/img'))
+        .pipe(gulp.dest('dist/img'))
+})
+
+gulp.task('processhtml' , function() {
+    return gulp.src('src/*.html')
+        .pipe(processhtml())
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('dist'))
+})
+
+gulp.task('move-styles' , function() {
+    return gulp.src('src/temp/css/*.css')
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest('dist/css'))
+})
+
+gulp.task('move-scripts' , function() {
+    return gulp.src('src/temp/js/*.js')
+        .pipe(rename('script.min.js'))
+        .pipe(gulp.dest('dist/js'))
 })
 
 gulp.task('clean-dist' , function() {
@@ -71,6 +94,6 @@ gulp.task('watch', ['sass' , 'babel', 'browserSync'], function (){
 
 gulp.task('dist' , function() {
     runSequence(
-        'clean-temp', 'clean-dist', 'babel', 'sass', 'autoprefixer', 'images'
+        'clean-dist', 'babel-uglify', 'sass-minify', 'autoprefixer', 'images', 'move-styles', 'move-scripts', 'processhtml', 'clean-temp'
     )
 })
